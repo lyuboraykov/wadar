65d43340a7e9cdc776eca65449a43a3fbcb90cc1
Lyubomir Raykov <raykovl@uber.com>
2016-06-30 22:25:26 +0300

    Add config.php

---
+++ public_html/config.php
@@ -0,0 +1,13 @@
+<?php
+
+class Config {
+
+  public static $SITE_FN = 61735; //can be used bellow
+  public static $SITE_CREATOR = "Lyubomir Raykov";
+  public static $SITE_ADMIN_EMAIL = "lyubo.raykov@gmail.com";
+  public static $SITE_INFO = "This project was created during 2016 year, on Web Technologies, Sofia University, FMI";
+
+  public static $DB_URL="https://project-8514226309654744846.firebaseio.com";
+  public static $SITE_DESCRIPTION="Technological radar";
+}
+?>
--- public_html/lib/firebase.php
+++ public_html/lib/firebase.php
@@ -1,11 +1,12 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../config.php';
 
 use Kreait\Firebase\Configuration;
 use Kreait\Firebase\Firebase;
 
 function getFirebase() {
-    $URL = "https://project-8514226309654744846.firebaseio.com";
+    $URL = Config::$DB_URL;
 
     $firebase = new Firebase($URL);
 
